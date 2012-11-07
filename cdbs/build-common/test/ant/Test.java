c8289d1efabee369bf46f4015db83123e1dd0935
Caio Begotti <caio1982@gmail.com>
2012-11-07 17:02:56 -0200

    removing the old checkout of cdbs source as it will be cloned every time by the script

--- cdbs/build-common/test/ant/Test.java
+++
@@ -1,5 +0,0 @@
-public class Test {
-	public int foo() {
-		return 0;
-	}
-}
--- cdbs/build-common/test/distutils/src/foo.py
+++
@@ -1,8 +0,0 @@
-class foo:
-  def __init__(self):
-    self.name = "JBailey"
-    self.id = 555
-
-  def output(self):
-    print self.name
-    print self.id

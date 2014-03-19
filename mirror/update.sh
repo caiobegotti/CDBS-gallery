191acfa56413c7c3fd78eca349c78292c1f9e2b1
Caio Begotti <caio1982@gmail.com>
2014-03-19 08:56:51 -0300

    leaner, with the archs variable fixed as well, my bad

--- mirror/update.sh
+++ mirror/update.sh
@@ -1,10 +1,6 @@
-#!/bin/bash -e
-# gpgv: can't check signature: public key not found
-# cp /usr/share/keyrings/debian-archive-keyring.gpg ~/.gnupg/trustedkeys.gpg
-# gpg --keyring /usr/share/keyrings/debian-archive-keyring.gpg --export | gpg --no-default-keyring --keyring trustedkeys.gpg --import
-# gpg --keyserver hkp://subkeys.pgp.net --recv-keys A040830F7FAC5991
+#!/bin/bash -xev
 
-mirror=ftp.us.debian.org
+mirror=ftp.br.debian.org
 distributions=jessie
 archs=i386,amd64,all
 sections=main,contrib,non-free
@@ -16,7 +12,7 @@ function update_mirror() {
 	archs=${3}
 	sections=${4}
 	target=${5}
-	$(which debmirror) --verbose --progress \
+	$(which debmirror) --debug --verbose --progress \
 	--host=${mirror:=ftp.us.debian.org} \
 	--dist=${distributions:=stable} \
 	--arch=${archs:=i386} \
@@ -33,15 +29,12 @@ function update_mirror() {
 }
 
 # regular mirror
-update_mirror ${mirror} ${distributions} ${archs} ${sections} ${target}
+#update_mirror ${mirror} ${distributions} ${archs} ${sections} ${target}
 
-mirror=ftp.br.debian.org
+# just "non-official" packages now and their binaries
 distributions=jessie-updates,jessie-backports
 archs=i386,amd64
-sections=main,contrib,non-free
-target=debian
 
-# just "non-official" packages now, only binaries
-update_mirror ${mirror} ${distributions} ${arch} ${sections} ${target}
+update_mirror ${mirror} ${distributions} ${archs} ${sections} ${target}
 
 exit 0

2b2544e74f413a42603939d6cb725544af93824c
Caio Begotti <caio1982@gmail.com>
2014-05-13 08:38:43 -0300

    i think i should put it along the other one-off scripts in the other repository but anyway... it makes possible to mirror all past debian releases with a single script and build the gallery for them

--- mirror/update.sh
+++ mirror/update.sh
@@ -1,40 +1,42 @@
-#!/bin/bash -e
+#!/bin/bash -xv
+# gpgv: can't check signature: public key not found
+# cp /usr/share/keyrings/debian-archive-keyring.gpg ~/.gnupg/trustedkeys.gpg
+# gpg --keyring /usr/share/keyrings/debian-archive-keyring.gpg --export | gpg --no-default-keyring --keyring trustedkeys.gpg --import
+# gpg --keyserver hkp://subkeys.pgp.net --recv-keys A040830F7FAC5991
 
+archive_mirror=archive.debian.org
 mirror=ftp.br.debian.org
-distributions=jessie
-archs=i386,amd64,all
-sections=main,contrib,non-free
-target=debian
 
-function update_mirror() {
-	mirror=${1}
-	distributions=${2}
-	archs=${3}
-	sections=${4}
-	target=${5}
-	$(which debmirror) --debug --verbose --progress \
-	--host=${mirror:=ftp.us.debian.org} \
-	--dist=${distributions:=stable} \
-	--arch=${archs:=i386} \
-	--section=${sections:=main} \
-	--method=rsync \
-	--getcontents \
-	--progress \
-	--nosource \
-	--ignore-missing-release \
-	--ignore-release-gpg \
-	--ignore-small-errors \
-	--root=:debian \
-	${target:=debian}/
+# FIXME: lenny's archive is asking for an anonymous password
+archive_distributions=etch,sarge,woody
+distributions=sid,jessie,wheezy,squeeze
+
+archive_sections=main,contrib,non-free
+sections=main,contrib,non-free
+
+archive_method=http
+method=rsync
+
+function _debmirror() {
+	debmirror \
+		--host=${1} \
+		--dist=${2} \
+		--section=${3} \
+		--method=${4} \
+		--getcontents \
+		--source \
+		--progress \
+		--arch=none \
+		--no-check-gpg \
+		--ignore-missing-release \
+		--ignore-release-gpg \
+		--ignore-small-errors \
+		--root=:debian \
+		debian/
 }
 
-# regular mirror
-update_mirror ${mirror} ${distributions} ${archs} ${sections} ${target}
+_debmirror ${archive_mirror} ${archive_distributions} ${archive_sections} ${archive_method}
 
-# just "non-official" packages now and their binaries
-distributions=jessie-updates,jessie-backports
-archs=i386,amd64
-
-update_mirror ${mirror} ${distributions} ${archs} ${sections} ${target}
+_debmirror ${mirror} ${distributions} ${sections} ${method}
 
 exit 0
